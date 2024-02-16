e6402e6ae1434c497b41ab9cbfe12a721f013658
vijay <128715498+vijaykumar-143@users.noreply.github.com>
2024-02-16 07:31:04 +0530

    Update daily_life.py

--- daily_life.py
+++ daily_life.py
@@ -6,50 +6,50 @@ f=input()
 d=input()
 ss=input()
 if(a<=6):
-    print("go to study & get ready");
+    print("good moring have a nice day it's 6'o' clock, go to study & get ready");
     if(r =="yes"):
-        print("pack box and go to busstop");
+        print("pack box and go to busstop before 8 AM");
         if(b>=8):
-            print("sit in bus")
+            print("select your seat and sit in bus ")
             if(c<=9):
-                print("enter to college")
+                print("enter to college &go to class before 9:05AM")
                 if(cc=="yes"):
-                    print("listen to classes")
+                    print("listen to classes attentively from 9AM to 12:40PM")
                     if(l=="yes"):
-                        print("have lunch and listen next classes")
+                        print("have lunch before 1:30PM and listen to classes upto 5PM")
                         if(g<=5):
-                            print("go and sit in the bus")
+                            print(" you reached college bus stand before 5:10 take your seat in the bus")
                             if(h<=6):
-                                print("go to home")
+                                print("go to home and have your works")
                                 if(f=="yes"):
-                                    print("have dinner")
+                                    print("first get freshup before 7:30 PM and have your  dinner ")
                                     if(d=="yes"):
-                                        print("go to study")
+                                        print("after having your dinner study for 2 hours from 8 PM to 10PM")
                                         if(s>=2):
-                                            print("go to sleep")
+                                            print("go to sleep after study")
                                             if(ss=="yes"):
-                                                print("go to sleep & good night")
+                                                print("it's 10P< go to sleep & good night")
                                             else:
-                                                print("don't sleep")
+                                                print("don't sleep your work is not completed")
                                         else:
                                             print("complete 2 hours study")
                                     else:
-                                        print("have your dinner first")
+                                        print("have your dinner first before 7:30/8PM")
                                 else:
-                                    print("first go and freshup")
+                                    print("first go and freshup ")
                             else:
-                                print("go to home quickly")
+                                print("go to home before 6:20PM to complete your works")
                         else:
-                            print("go to busstop fastly")
+                            print("reach college bus stand before 5:10 to get a seat in the bus")
                     else:
-                        print("first have your lunch")
+                        print("first have your lunch in time to attend classes")
                 else:
-                    print("go to class in time")
+                    print("go to class in time to get attendence")
             else:
-                print("go to college in time")
+                print("go to college in time ,it's late come to college tommarow")
         else:
-            print("go to busstop before 8")
+            print("go to busstop before 8,bus already left")
     else:
-        print("get ready in time")
+        print("get ready in time ,it's time to go to bus stop")
 else: 
-    print("wakeup before 6 am")
+    print("wakeup it's 6AM ,this is not good to sleep after 6 AM")
